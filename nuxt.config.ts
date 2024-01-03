ccb82c2efec486c2e84a7ad95dc8db1a8d13c657
Vasileios Bakas <vasileios.bakas92@gmail.com>
2024-01-03 13:35:44 +0200

    Added social icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
-  extends: '@nuxt-themes/typography',
-  typescript: { shim: false },
-  devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
-  tailwindcss: {
-    cssPath: '~/assets/css/tailwind.css',
-    configPath: 'tailwind.config',
-    viewer: true,
-  },
-  content: {
-    highlight: {
-      theme: 'nord',
-      preload: ['ts', 'js', 'json', 'vue', 'bash', 'html'],
-    },
-  },
+	ssr: false,
+	extends: '@nuxt-themes/typography',
+	typescript: { shim: false },
+	devtools: { enabled: false },
+	modules: ['@nuxtjs/tailwindcss', '@nuxt/content', 'nuxt-icon'],
+	tailwindcss: {
+		cssPath: '~/assets/css/tailwind.css',
+		configPath: 'tailwind.config',
+		viewer: true,
+	},
+	content: {
+		highlight: {
+			theme: 'nord',
+			preload: ['ts', 'js', 'json', 'vue', 'bash', 'html'],
+		},
+	},
 });
